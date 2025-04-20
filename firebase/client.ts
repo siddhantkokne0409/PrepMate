264bfb361a6a9da5701538a208b6ccdf72c74097
Siddhant <koknesiddhant3@gmail.com>
2025-04-20 14:57:26 +0530

    env changed

--- firebase/client.ts
+++ firebase/client.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
-  authDomain: process.env.FIREBASE_CLIENT_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_CLIENT_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_CLIENT_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_CLIENT_APP_ID,
-  measurementId: process.env.FIREBASE_CLIENT_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
